f8661171d82af89d28d8a3dc493655a70d8d3327
tuyet-pham <tiipham19@gmail.com>
2020-02-25 04:00:04 -0600

    Added the welcome page

--- Kaught1.0/py/aesthetics.py
+++ Kaught1.0/py/aesthetics.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python3
 import tkinter as tk
 from tkinter import Label, Entry
-from tkinter import Frame, Button
-from tkinter import TOP, LEFT, RIGHT, BOTTOM, RIGHT
+from tkinter import Frame, Button, Grid
+from tkinter import TOP, LEFT, RIGHT, BOTTOM, Y, BOTH, END
 from tkinter import TRUE, FALSE
 from tkinter import Toplevel, Message
 from tkinter import PanedWindow
+from tkinter import Text, Scrollbar
 from tkinter import PhotoImage
+
+kaughtmsg = """
+Hello user!
+
+It's always good to stay organized and remain 
+kaught up in your everyday agenda! I made Kaught so you don't have to :)
+User registry is only to be stored within your own machine, so fret not. 
+This program is open sourced so feel free to grab.
+Let me know if any improvements can be made. 
+
+@github.com/tuyet-pham
+@tiidev19@gmail.com
+
+"""
 
 
 class KButtonLight(Button):
@@ -15,10 +30,9 @@ class KButtonLight(Button):
         self['bg'] = 'whitesmoke'
         self['fg'] = 'black'
         self['relief'] = 'flat'
-        self['width'] = 10
+        self['width'] = 15
         self['borderwidth'] = 0
         self['highlightthickness'] = 0
-        self['pady'] = 5
         self['activebackground'] = 'gray24'
         self['activeforeground'] = 'whitesmoke'
 
@@ -28,16 +42,15 @@ class KButtonDark(Button):
         self['bg'] = 'gray24'
         self['fg'] = 'white'
         self['relief'] = 'flat'
-        self['width'] = 10
+        self['width'] = 15
         self['borderwidth'] = 0
         self['highlightthickness'] = 0
-        self['pady'] = 5
 
 
 class KLogo(Label):
     def __init__(self, *args, **kwargs):
         Label.__init__(self, *args, **kwargs)
-        self["photo"] = PhotoImage(file="kaught_logo300.png")
-        self["image"] = photo
-        self["pady"] = 5
-        self["padx"] = 5
+        self.photo = PhotoImage(file="../public/kaught_logo200.png")
+        self['image'] = self.photo
+
+
--- Kaught1.0/setup/udata.py
+++ Kaught1.0/setup/udata.py
@@ -2,22 +2,26 @@
 import os
 import sys
 sys.path.append('../py')
-sys.path.append('../public')
 from aesthetics import *
 
-class Application(tk.Frame):
-    def __init__(self, master):
+class Setup(tk.Frame):
+    def __init__(self, master, width="500", height="600"):
         super().__init__(master)
         self.master = master
-
         self.pack()
         self.create_widgets()
         self.setupUser()
 
     def create_widgets(self):
-        self.klogo = KLogo(self).pack(side=TOP)
-        self.exitButton = KButtonLight(self, text="Quit").pack(side=LEFT)
-        self.nextButton = KButtonDark(self, text="Next").pack(side=RIGHT)
+        self.klogo = KLogo(self).pack(side=TOP,pady=20)
+
+        self.kaughtM = Text(self, height=15, width=50, relief='flat')
+        self.kaughtM.insert(tk.END, kaughtmsg)
+        self.kaughtM.pack(side=TOP, fill=Y, pady=20)    
+
+        self.exitButton = KButtonLight(self, text="Quit", command=quit).pack(side=RIGHT, pady=50,padx=2)
+        self.nextButton = KButtonDark(self, text="Next", command=self.setupUser).pack(side=LEFT, pady=50, padx=2)        
+
 
     def setupUser(self):
         print("Hey")
@@ -26,6 +30,6 @@ class Application(tk.Frame):
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("Kaught1.0 Setup")
-    root.geometry("500x600")    
-    app = Application(master=root)
+    root.geometry("500x600")
+    app = Setup(master=root)
     app.mainloop()
